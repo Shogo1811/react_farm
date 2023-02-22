a1c6c613eabb1b3a5548bc4bf29112059928b840
Shogo1811 <shogo.n.1118@gmail.com>
2023-02-23 01:23:13 +0900

    Get single Task

--- src/components/TaskItem.tsx
+++ src/components/TaskItem.tsx
@@ -6,13 +6,15 @@ import { setEditedTask } from '../slices/appSlice'
 import { useMutateTask } from '../hooks/useMutateTask'
 
 const TaskItemMemo: VFC<
-  Task
-> = ({ id, title, description }) => {
+  Task & {
+    setId: React.Dispatch<React.SetStateAction<string>>
+  }
+> = ({ id, title, description, setId }) => {
   const dispatch = useAppDispatch()
   const { deleteTaskMutation } = useMutateTask()
   return (
     <li>
-      <span className="font-bold cursor-pointer">
+      <span className="font-bold cursor-pointer" onClick={() => setId(id)}>
         {title}
       </span>
       <div className="flex float-right ml-20">
